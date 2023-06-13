3c989914e30abd99cb5e18405dc347648ba423eb
Roshan Wazare <roshanwazare@gmail.com>
2023-06-13 23:44:44 +0530

    added cron scheduler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import rateLimit from "express-rate-limit";
 import tweet from "./routes/tweet.js";
+import cron from "node-cron";
 import { executeScheduledTweets } from "./controllers/tweet.js";
 
 const app = express();
@@ -19,7 +20,10 @@ app.use("/api/", apiLimiter);
 
 app.use("/api/v1/tweet", tweet);
 
-// Moved as a CRON job
 // // script executed every 7 minutes.
+cron.schedule('*/7 * * * *', () => {
+    console.log("job scheduled cron")
+    executeScheduledTweets()
+})
 // setInterval(executeScheduledTweets, 420000);
 export default app ;
